9147029124dce93761cbe8b246afa9f2adda4dab
roryzoerb <rory.zoerb@gmail.com>
2017-08-27 08:00:17 -0600

    Completed Tutorial #23

--- nnvue2-21/src/components/Footer.vue
+++ nnvue2-21/src/components/Footer.vue
@@ -1,17 +1,22 @@
 <template>
     <footer>
-        <p>{{ copyright }}</p>
+        <p>{{ copyright }} {{ title }}</p>
     </footer>
 </template>
 
 <script>
 
 export default {
-  data () {
-    return {
-        copyright: 'Copyright 2017 Vue Ninjas'
+    props: {
+        title: {
+            type: String
+        }
+    },
+    data () {
+        return {
+            copyright: 'Copyright 2017'
+        }
     }
-  }
 }
 </script>
 
