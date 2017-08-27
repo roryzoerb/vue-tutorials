4026e1ca8d20d85dd0c26331fb6fa0bcf6349eab
roryzoerb <rory.zoerb@gmail.com>
2017-08-27 16:35:10 -0600

    Completed Tutorial #27

--- nnvue2-27/src/App.vue
+++ nnvue2-27/src/App.vue
@@ -1,60 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <form-helper>
+      <div slot="form-header">
+        <h3>Form Header</h3>
+        <p>Parargraph for the form header...</p>
+      </div>
+      <div slot="form-fields">
+        <div>
+          <label for="name">Name:</label>
+          <input id="name" type="text" placeholder="Name" required/>
+        </div>
+        <div>
+          <label for="pwd">Password:</label>
+          <input id="pwd" type="password" placeholder="password" required/>
+        </div>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit()">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
+import formHelper from "./components/formHelper.vue"
 export default {
-  name: 'app',
+  components: {
+    "form-helper": formHelper
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: "Slots Title from data()"
     }
+  },
+  methods: {
+     handleSubmit: function() {
+       alert("Submitted!");
+     }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
