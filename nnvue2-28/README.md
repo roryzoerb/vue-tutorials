9fe61a0b1cc99fe43a5ad00a9e22b5d37e0466c2
roryzoerb <rory.zoerb@gmail.com>
2017-08-27 19:42:57 -0600

    Completed Tutorial #28

--- nnvue2-28/README.md
+++ nnvue2-28/README.md
@@ -1,12 +1,43 @@
-# nnvue2-28
+![Logo of the project](./../img/vue_logo.png)
 
-> A Vue.js project
+# vue-tutorial
+
+>#27 - Slots
+
+From the Net Ninja's [Vue JS 2 Tutorials](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa) on YouTube
+
+## My Dev Environmet
+
+* OS: Windows 10 Pro - 1607
+* IDE: Visual Studio Code - 1.15.1
+* CLI: Command Prompt
+* node: v6.11.1
+* npm: 5.3.0
+* vue-cli: 2.8.2
+* yarn: v0.27.5
 
 ## Build Setup
 
+Prerequisites: [Node.js](https://nodejs.org/en/) (>=4.x, 6.x preferred), npm version 3+ and [Git](https://git-scm.com/).
+
 ``` bash
+npm install -g vue-cli
+```
+
+### Usage ( [Vue-CLI](https://github.com/vuejs/vue-cli) )
+
+``` bash
+vue init <template-name> <project-name>
+```
+
+This Tutorial:
+
+``` bash
+# simple scaffolding
+vue init webpack-simple nnvue2-27
+
 # install dependencies
-npm install
+npm install (or Yarn)
 
 # serve with hot reload at localhost:8080
 npm run dev
--- nnvue2-28/src/App.vue
+++ nnvue2-28/src/App.vue
@@ -1,60 +1,40 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+      <!-- <form-one></form-one> -->
+      <!-- <form-two></form-two> -->
+        <keep-alive>
+            <component v-bind:is="component"></component>
+        </keep-alive>
+        <button v-on:click="component = 'form-one'">Show form one</button>
+        <button v-on:click="component = 'form-two'">Show form two</button>
+    </div>
 </template>
 
 <script>
+// Imports
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    components: {
+        'form-one': formOne,
+        'form-two': formTwo
+    },
+    data () {
+        return {
+            component: 'form-one'
+        }
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for submitting');
+        }
     }
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
 }
 </style>
